f1cbd57fac02ba470de19253c11c6e9a175352a7
tuh00755 <tuh00755@temple.edu>
2022-03-08 15:07:07 -0500

    fixing merge conflicts

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 
-from flask import Flask, render_template, url_for,redirect,request
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return render_template("pythonanywhere.html")
 
-@app.route('/')
+@app.route("/login", methods=["POST", "GET"])
 def main():
-    return render_template("index.html")
+    if request.method == "POST":
+        user = request.form["username"]
+        password = request.form["password"]
+        print(user + password)
+        return redirect(url_for("user", usr=user, password=password))
+    else:
+        return render_template("pythonanywhere.html")
 
+@app.route("/<user> & <password>")
+def user(user, password):
+    return f"<h1>{user}{password}</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
