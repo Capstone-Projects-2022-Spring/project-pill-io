daca89c90afee66ab6caade011f5a69978f9a727
tuh00755 <tuh00755@temple.edu>
2022-03-14 18:01:27 -0400

    working (backup db plus login)

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
 from flask import Flask, render_template, url_for,redirect,request
+from flask import Blueprint, render_template, flash
+from flask_login import login_required, current_user
+from __init__ import create_app, db
 
-app = Flask(__name__)
+# our main blueprint
+main = Blueprint('main', __name__)
 
+@main.route('/') # home page that return 'index'
+def index():
+    return render_template('index.html')
 
-@app.route('/')
-def main():
-    return render_template("index.html")
+@main.route('/profile') # profile page that return 'profile'
+@login_required
+def profile():
+    return render_template('profile.html')
 
-@app.route('/login')
-def login():
-    if request.method == 'POST':
-        return render_template('userManage.html')
-    else:
-        return render_template('index.html')
-
-@app.route('/register')
-def register():
-    return render_template("userManage.html")
-
-@app.route('/help')
+@main.route('/help') # profile page that return 'profile'
+@login_required
 def help():
-    return render_template("help.html")
+    return render_template('help.html')
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+app = create_app() # we initialize our flask app using the __init__.py function
+if __name__ == '__main__':
+    db.create_all(app = create_app()) # create the SQLite database
+    app.run(debug=True) # run the flask app on debug mode
