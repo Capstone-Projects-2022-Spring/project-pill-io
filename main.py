834c08da3d485207bf027bc356d0660bdae67331
tuh00755 <tuh00755@temple.edu>
2022-03-26 03:49:40 -0400

    Initial additions to settings page

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, url_for,redirect,request
 from flask import Blueprint, render_template, flash
 from flask_login import login_required, current_user
 from models import User
+from flask_change_password import ChangePassword, ChangePasswordForm, SetPasswordForm
 from __init__ import create_app, db
 
 
@@ -29,7 +30,7 @@ def help():
 
 app = create_app() # we initialize our flask app using the __init__.py function
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=7002, debug=True)
+    #app.run(host='0.0.0.0', port=7002, debug=True)
 
     db.create_all(app = create_app()) # create the SQLite database
     app.run(debug=True) # run the flask app on debug mode
